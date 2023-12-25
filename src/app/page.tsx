4d33aa08e506002be026a95e74659b52a34711bd
unknown <matthewanh@hotmail.com>
2023-12-25 15:49:31 -0800

    set up a new fault font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import { useState } from "react"
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-5xl">Hello</h1>
+      <h1 className="text-5xl">abcdefghijklmnopqrstuvwxyz</h1>
+      <h1 className="text-5xl">ABCDEFGHIJKLMNOPQRSTUVWXYZ</h1>
+      <h1 className="font-extrabold text-5xl">ABCDEFGHIJKLMNOPQRSTUVWXYZ</h1>
+      <h1 className="text-5xl">Hellow World!</h1>
+
+      <TextBox />
+    </main>
+  )
+}
+
+function TextBox() {
+  const [text, setName] = useState('');
+
+  return (
+    <main>
+      <input className="border-sky-100 border-2" type="text" name="" id="" />
     </main>
   )
 }
